00dbd238f7afaf45a86a245d8c4609be610b1051
Luis Neto <santosneto.luis@gmail.com>
2023-03-11 16:53:13 -0300

    NewTransactionModal Used

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,32 @@ import { Dashbord } from "./components/Dashborder";
 import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 
+import Modal from "react-modal";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement("#root");
+
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashbord />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyles />
     </>
   );
