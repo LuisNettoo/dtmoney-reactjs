239cb56109c82b7aae0a498d734d43a696d6cbe4
Luis Neto <santosneto.luis@gmail.com>
2023-03-11 16:50:53 -0300

    API Miragejs Config

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer de site",
+          type: "deposit",
+          category: "Desenvolvimento",
+          amount: 6000,
+          createdAt: new Date("2023-03-11 16:24:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 8000,
+          createdAt: new Date("2023-03-11 16:24:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
     });
   },
 });
