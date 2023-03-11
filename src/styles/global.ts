732e96d71b0674ea25ec89bd2880d0fdf8477a7c
Luis Neto <santosneto.luis@gmail.com>
2023-03-11 16:53:21 -0300

    Modal styled

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
   
@@ -53,4 +53,41 @@ export const GlobalStyles = createGlobalStyle`
     cursor: not-allowed;
   }
 
-`
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background-color: var(--background);
+    outline: none;
+
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background-color: transparent;
+    transition: filter 200ms;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+`;
